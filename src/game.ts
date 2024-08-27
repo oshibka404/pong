94d3d282398d3542d79be775abd770c6a1dac3a5
Andrey Ozornin <andrey@ozorn.in>
2024-08-27 23:41:28 +0200

    remove unused property

--- src/game.ts
+++ src/game.ts
@@ -4,12 +4,9 @@ import { StartMenu } from "./screens/start-menu";
 type GameState = 'not_started' | 'playing'
 
 export class Game {
-    private state: GameState = 'not_started'
     constructor(private startMenu: StartMenu, private match: Match) {}
 
     private setState(state: GameState) {
-        this.state = state
-        
         if (state === 'not_started') {
             this.startMenu.show()
             this.match.hide()
